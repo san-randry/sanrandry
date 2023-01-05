c81185b24fdadcaeeb63f95b7ebbd2bd3d175ae5
sanrandry <sinrandry@gmail.com>
2023-01-05 15:24:29 +0300

    add index file

---
+++ index.js
@@ -0,0 +1,15 @@
+function log(msg, options = { loudness: 'default' }) {
+  if (options && options.loudness) {
+    switch (options.loudness) {
+      case 'shout':
+        if (typeof msg === str) console.log(msg.toUpperCase());
+        break;
+      case 'whisper':
+        if (typeof msg === str) console.log(msg.toLowerCase());
+        break;
+      default:
+        console.log(msg);
+    }
+  }
+}
+module.exports = log;
---
+++ test-directory/index.js
@@ -0,0 +1,2 @@
+const log = require('sanrandry');
+log('Hello World', { loudness: 'shout' });
