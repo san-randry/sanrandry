15b81a1ee965b867858de4c4cdc7769b1c8830fc
sanrandry <sinrandry@gmail.com>
2023-01-05 15:43:49 +0300

    fix : bug strig type

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function log(msg, options = { loudness: 'default' }) {
   if (options && options.loudness) {
     switch (options.loudness) {
       case 'shout':
-        if (typeof msg === str) console.log(msg.toUpperCase());
+        if (typeof msg === 'string') console.log(msg.toUpperCase());
         break;
       case 'whisper':
-        if (typeof msg === str) console.log(msg.toLowerCase());
+        if (typeof msg === 'string') console.log(msg.toLowerCase());
         break;
       default:
         console.log(msg);
